6156a0482b26f5bd860839c65a1a856d834eb12b
Peter Kelly <peter@uxproductivity.com>
2015-05-06 22:39:16 +0700

    Use clearer variable names in delta algorithm

--- hyperdns/netdns/delta.py
+++ hyperdns/netdns/delta.py
@@ -1,68 +1,68 @@
 from hyperdns.netdns.resolutiontree import *
 
-def describe_rtree_delta(tree1,tree2):
+def describe_rtree_delta(source_tree,target_tree):
 
     messages = []
 
-    def changed(msg):
+    def record_change_text_message(msg):
         assert isinstance(msg,str)
         messages.append(msg)
 
-    def gen_delta(node1,node2,indent = ''):
-        if node1 == None and node2 == None:
+    def gen_delta(source_node,target_node,indent = ''):
+        if source_node == None and target_node == None:
             return
 
-        if node1 == None:
-            if isinstance(node2,GeoNode):
-                changed('%sAdd Geo node'%(indent))
-                gen_delta(GeoNode({}),node2,indent)
+        if source_node == None:
+            if isinstance(target_node,GeoNode):
+                record_change_text_message('%sAdd Geo node'%(indent))
+                gen_delta(GeoNode({}),target_node,indent)
                 return
-            elif isinstance(node2,WeightedNode):
-                changed('%sAdd Weighted node'%(indent))
-                gen_delta(WeightedNode([]),node2,indent)
+            elif isinstance(target_node,WeightedNode):
+                record_change_text_message('%sAdd Weighted node'%(indent))
+                gen_delta(WeightedNode([]),target_node,indent)
                 return
-            elif isinstance(node2,RecordGroupNode):
-                changed('%sAdd Record Set'%(indent))
-                gen_delta(RecordGroupNode([]),node2,indent)
+            elif isinstance(target_node,RecordGroupNode):
+                record_change_text_message('%sAdd Record Set'%(indent))
+                gen_delta(RecordGroupNode([]),target_node,indent)
                 return
-            elif isinstance(node2,RecordNode):
-                gen_delta_records(None,node2,indent)
-                return
-            else:
-                raise TypeError('Unknown node type: %s'%(node2.__class__.__name__))
-
-        elif node2 == None:
-            if isinstance(node1,GeoNode):
-                gen_delta(node1,GeoNode({}),indent)
-                changed('%sDel Geo node'%(indent))
-                return
-            elif isinstance(node1,WeightedNode):
-                gen_delta(node1,WeightedNode([]),indent)
-                changed('%sDel Weighted node'%(indent))
-                return
-            elif isinstance(node1,RecordGroupNode):
-                gen_delta(node1,RecordGroupNode([]),indent)
-                changed('%sDel Record Set'%(indent))
-                return
-            elif isinstance(node1,RecordNode):
-                gen_delta_records(node1,None,indent)
+            elif isinstance(target_node,RecordNode):
+                gen_delta_records(None,target_node,indent)
                 return
             else:
-                raise TypeError('Unknown node type: %s'%(node1.__class__.__name__))
+                raise TypeError('Unknown node type: %s'%(target_node.__class__.__name__))
 
-        elif node1.__class__ != node2.__class__:
-            gen_delta(node1,None,indent)
-            gen_delta(None,node2,indent)
+        elif target_node == None:
+            if isinstance(source_node,GeoNode):
+                gen_delta(source_node,GeoNode({}),indent)
+                record_change_text_message('%sDel Geo node'%(indent))
+                return
+            elif isinstance(source_node,WeightedNode):
+                gen_delta(source_node,WeightedNode([]),indent)
+                record_change_text_message('%sDel Weighted node'%(indent))
+                return
+            elif isinstance(source_node,RecordGroupNode):
+                gen_delta(source_node,RecordGroupNode([]),indent)
+                record_change_text_message('%sDel Record Set'%(indent))
+                return
+            elif isinstance(source_node,RecordNode):
+                gen_delta_records(source_node,None,indent)
+                return
+            else:
+                raise TypeError('Unknown node type: %s'%(source_node.__class__.__name__))
+
+        elif source_node.__class__ != target_node.__class__:
+            gen_delta(source_node,None,indent)
+            gen_delta(None,target_node,indent)
             return
 
-        elif isinstance(node1,GeoNode) and isinstance(node2,GeoNode):
-            regions1 = node1.entries.keys()
-            regions2 = node2.entries.keys()
+        elif isinstance(source_node,GeoNode) and isinstance(target_node,GeoNode):
+            regions1 = source_node.entries.keys()
+            regions2 = target_node.entries.keys()
             deleted = regions1 - regions2
             added = regions2 - regions1
             kept = regions1 & regions2
-            regions_by_children1 = { node1.entries[r].child: r for r in node1.entries.keys() }
-            regions_by_children2 = { node2.entries[r].child: r for r in node2.entries.keys() }
+            regions_by_children1 = { source_node.entries[r].child: r for r in source_node.entries.keys() }
+            regions_by_children2 = { target_node.entries[r].child: r for r in target_node.entries.keys() }
 
             for child in regions_by_children1.keys():
                 region1 = regions_by_children1[child]
@@ -70,16 +70,16 @@ def describe_rtree_delta(tree1,tree2):
                 if region2 != None and region2 != region1:
                     # The children of the region entries are exactly the same, so
                     # we don't need to traverse those. All we have to do is indicate
-                    # that the region codes have changed.
-                    changed('Rename %s -> %s'%(region1,region2))
+                    # that the region codes have record_change_text_message.
+                    record_change_text_message('Rename %s -> %s'%(region1,region2))
 
-                    cname1 = node1.entries[region1].cname
-                    cname2 = node2.entries[region2].cname
+                    cname1 = source_node.entries[region1].cname
+                    cname2 = target_node.entries[region2].cname
 
                     if cname1 != cname2:
                         cstr1 = '%s %d'%(cname1.rdata,cname1.ttl) if cname1 != None else None
                         cstr2 = '%s %d'%(cname2.rdata,cname2.ttl) if cname2 != None else None
-                        changed('%sRegion "%s": Change CNAME %s -> %s'%(indent,region2,cstr1,cstr2))
+                        record_change_text_message('%sRegion "%s": Change CNAME %s -> %s'%(indent,region2,cstr1,cstr2))
 
                     deleted.remove(region1)
                     added.remove(region2)
@@ -87,37 +87,37 @@ def describe_rtree_delta(tree1,tree2):
             for region in sorted(deleted):
                 # indent2 = '%s%-5s'%(indent,str(region)+': ')
                 indent2 = '%sRegion "%s": '%(indent,str(region))
-                gen_delta(node1.entries[region].child,None,indent2)
-                changed('%sDel region %s'%(indent,region))
+                gen_delta(source_node.entries[region].child,None,indent2)
+                record_change_text_message('%sDel region %s'%(indent,region))
 
             for region in sorted(added):
-                changed('%sAdd region %s'%(indent,region))
+                record_change_text_message('%sAdd region %s'%(indent,region))
                 # indent2 = '%s%-5s'%(indent,str(region)+': ')
                 indent2 = '%sRegion "%s": '%(indent,str(region))
-                gen_delta(None,node2.entries[region].child,indent2)
+                gen_delta(None,target_node.entries[region].child,indent2)
 
             for region in sorted(kept):
                 # indent2 = '%s%-5s'%(indent,str(region)+': ')
                 indent2 = '%sRegion "%s": '%(indent,region)
 
-                cname1 = node1.entries[region].cname
-                cname2 = node2.entries[region].cname
+                cname1 = source_node.entries[region].cname
+                cname2 = target_node.entries[region].cname
 
                 if cname1 != cname2:
                     cstr1 = '%s %d'%(cname1.rdata,cname1.ttl) if cname1 != None else None
                     cstr2 = '%s %d'%(cname2.rdata,cname2.ttl) if cname2 != None else None
-                    changed('%sRegion "%s": Change CNAME %s -> %s'%(indent,region,cstr1,cstr2))
+                    record_change_text_message('%sRegion "%s": Change CNAME %s -> %s'%(indent,region,cstr1,cstr2))
 
-                gen_delta(node1.entries[region].child,node2.entries[region].child,indent2)
+                gen_delta(source_node.entries[region].child,target_node.entries[region].child,indent2)
 
             return
-        elif isinstance(node1,WeightedNode) and isinstance(node2,WeightedNode):
+        elif isinstance(source_node,WeightedNode) and isinstance(target_node,WeightedNode):
             index = 0
-            while index < len(node1.entries) and index < len(node2.entries):
-                entry1 = node1.entries[index]
-                entry2 = node2.entries[index]
+            while index < len(source_node.entries) and index < len(target_node.entries):
+                entry1 = source_node.entries[index]
+                entry2 = target_node.entries[index]
                 if entry1.weight != entry2.weight:
-                    changed('%sGroup %d: Change weight %d%% -> %d%%'%(
+                    record_change_text_message('%sGroup %d: Change weight %d%% -> %d%%'%(
                         indent,
                         index+1,
                         int(100*entry1.weight),
@@ -129,48 +129,48 @@ def describe_rtree_delta(tree1,tree2):
                 if cname1 != cname2:
                     cstr1 = '%s %d'%(cname1.rdata,cname1.ttl) if cname1 != None else None
                     cstr2 = '%s %d'%(cname2.rdata,cname2.ttl) if cname2 != None else None
-                    changed('%sGroup %d: Change CNAME %s -> %s'%(indent,index+1,cstr1,cstr2))
+                    record_change_text_message('%sGroup %d: Change CNAME %s -> %s'%(indent,index+1,cstr1,cstr2))
 
                 if entry1.child.all_records != entry2.child.all_records:
                     indent2 = '%sGroup %d: '%(indent,index+1)
-                    gen_delta_records(node1,node2,indent2)
+                    gen_delta_records(source_node,target_node,indent2)
                 index += 1
-            deli = len(node1.entries)-1
+            deli = len(source_node.entries)-1
             while deli >= index:
                 indent2 = '%sGroup %d: '%(indent,deli+1)
-                gen_delta_records(node1,None,indent2)
-                changed('%sDel group %d, weight %2.1f%%'%(indent,deli+1,100*node1.entries[deli].weight))
+                gen_delta_records(source_node,None,indent2)
+                record_change_text_message('%sDel group %d, weight %2.1f%%'%(indent,deli+1,100*source_node.entries[deli].weight))
                 deli -= 1
             addi = index
-            while addi < len(node2.entries):
-                changed('%sAdd group %d, weight %2.1f%%'%(indent,addi+1,100*node2.entries[addi].weight))
+            while addi < len(target_node.entries):
+                record_change_text_message('%sAdd group %d, weight %2.1f%%'%(indent,addi+1,100*target_node.entries[addi].weight))
                 indent2 = '%sGroup %d: '%(indent,addi+1)
-                gen_delta_records(None,node2,indent2)
+                gen_delta_records(None,target_node,indent2)
                 addi += 1
 
             return
-        elif isinstance(node1,RecordGroupNode) and isinstance(node2,RecordGroupNode):
-            gen_delta_records(node1,node2,indent)
+        elif isinstance(source_node,RecordGroupNode) and isinstance(target_node,RecordGroupNode):
+            gen_delta_records(source_node,target_node,indent)
             return
-        elif isinstance(node1,RecordNode) and isinstance(node2,RecordNode):
-            gen_delta_records(node1,node2,indent)
+        elif isinstance(source_node,RecordNode) and isinstance(target_node,RecordNode):
+            gen_delta_records(source_node,target_node,indent)
             return
 
         raise ValueError('Case is not covered (this should not happen) - %s and %s'%(
-                         node1.__class__.__name__,node2.__class__.__name__))
+                         source_node.__class__.__name__,target_node.__class__.__name__))
 
-    def gen_delta_records(node1,node2,indent = ''):
-        records1 = node1.all_records if node1 != None else frozenset()
-        records2 = node2.all_records if node2 != None else frozenset()
+    def gen_delta_records(source_node,target_node,indent = ''):
+        records1 = source_node.all_records if source_node != None else frozenset()
+        records2 = target_node.all_records if target_node != None else frozenset()
 
         deleted = records1 - records2
         added = records2 - records1
         kept = records1 & records2
 
         for record in sorted(deleted):
-            changed('%sDel record %s'%(indent,str(RecordNode(record))))
+            record_change_text_message('%sDel record %s'%(indent,str(RecordNode(record))))
         for record in sorted(added):
-            changed('%sAdd record %s'%(indent,str(RecordNode(record))))
+            record_change_text_message('%sAdd record %s'%(indent,str(RecordNode(record))))
 
-    gen_delta(tree1.root,tree2.root)
+    gen_delta(source_tree.root,target_tree.root)
     return messages
